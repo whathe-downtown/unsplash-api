6bd4f3d92c29c601db20bfab449e13fe786ab458
Chungkyu <dbcjdrb1997@gmail.com>
2022-01-21 17:34:47 +0900

    commit2

--- app/src/main/java/org/techtown/air/pollution/unsplash_api/MainActivity.kt
+++ app/src/main/java/org/techtown/air/pollution/unsplash_api/MainActivity.kt
@@ -1,7 +1,11 @@
 package org.techtown.air.pollution.unsplash_api
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.MainScope
@@ -19,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         initViews()
+        bindViews()
         fetchRandomPhotos()
     }
 
@@ -30,13 +35,52 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
         binding.recyclerView.adapter = PhotoAdapter()
     }
-    private fun fetchRandomPhotos(query: String? = null ) = scope.launch{
-        Repository.getRandomPhotos(query)?.let { photos->
-            (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
-                this.photos = photos
-                notifyDataSetChanged()
+    private fun bindViews() {
+        binding.searchEditText
+            .setOnEditorActionListener { editText, actionId, event ->
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    currentFocus?.let { view ->
+                        val inputMethodManager =
+                            getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                        inputMethodManager?.hideSoftInputFromWindow(
+                            view.windowToken,
+                            0
+                        ) // keyobrad gone
+
+                        view.clearFocus()
+                    }
+
+                    fetchRandomPhotos(editText.text.toString())
+                }
+
+                true
             }
+        binding.refreshLayout.setOnRefreshListener {
+            fetchRandomPhotos(binding.searchEditText.toString())
+        }
+    }
+
+    private fun fetchRandomPhotos(query: String? = null ) = scope.launch{
+        try {
+        Repository.getRandomPhotos(query)?.let { photos->
+
+                binding.errorDescriptionTextView.visibility = View.GONE
+                (binding.recyclerView.adapter as? PhotoAdapter)?.apply {
+                    this.photos = photos
+                    notifyDataSetChanged()
+                    }
+                }
+                binding.recyclerView.visibility = View.VISIBLE
+            }catch (exception : Exception){
+                binding.recyclerView.visibility = View.INVISIBLE
+                binding.errorDescriptionTextView.visibility = View.VISIBLE
+            }finally {
+                binding.shimmerLayout.visibility = View.GONE
+                binding.refreshLayout.isRefreshing   = false
+            }
+
+
+
         }
 
     }
-}
--- app/src/main/java/org/techtown/air/pollution/unsplash_api/PhotoAdapter.kt
+++ app/src/main/java/org/techtown/air/pollution/unsplash_api/PhotoAdapter.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import org.techtown.air.pollution.unsplash_api.data.models.PhotoResponse
 import org.techtown.air.pollution.unsplash_api.databinding.ItemPhotoBinding
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 
 class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
 
@@ -33,12 +34,32 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
     ): RecyclerView.ViewHolder(binding.root){
 
         fun bind(photo: PhotoResponse){
+            val dimensionRation= photo.height/photo.width.toFloat()  // height width nullable wprj
+            val targetWidth = binding.root.resources.displayMetrics.widthPixels -
+                    (binding.root.paddingStart + binding.root.paddingEnd)
+
+            val targetHeight = (targetWidth * dimensionRation).toInt()
+
+            binding.contentsContainer.layoutParams =
+                binding.contentsContainer.layoutParams.apply {
+                    height = targetHeight
+                }
+
             Glide.with(binding.root)
                 .load(photo.urls?.regular)
+                .thumbnail(
+                    Glide.with(binding.root)
+                        .load(photo.urls?.thumb)
+                        .transition(DrawableTransitionOptions.withCrossFade())
+                )
+                .override(targetWidth, targetHeight)
                 .into(binding.photoImageView)
 
             Glide.with(binding.root)
                 .load(photo.user?.profileImageUrls?.small)
+                .placeholder(R.drawable.shape_profile_placeholder)
+                .circleCrop()
+                .transition(DrawableTransitionOptions.withCrossFade())
                 .into(binding.profileImageView)
 
             if(photo.user?.name.isNullOrBlank()){
@@ -51,11 +72,12 @@ class PhotoAdapter : RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
             }
             if(photo.description.isNullOrBlank()){
                 binding.descriptionTextView.visibility = View.GONE
+            } else{
+                binding.descriptionTextView.visibility = View.VISIBLE
+                binding.descriptionTextView.text =photo.description
             }
-            binding.descriptionTextView.visibility = View.VISIBLE
+            }
 
-            binding.descriptionTextView.text =photo.description
-        }
 
     }
 }
